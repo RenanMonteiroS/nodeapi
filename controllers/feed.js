3132e6eac7a6ac91d452b7fe22a268f113f5f5c2
RenanMonteiroS <renanmonteirodes@gmail.com>
2024-09-17 12:56:11 -0300

    Fetch single post + all posts + edit a post and images

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,37 +1,65 @@
 const {validationResult} = require('express-validator');
+const fs = require('fs');
+const path = require('path');
 
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            _id: '1',
-            title: 'Post', 
-            content: "FirstPost", 
-            imageUrl: "images/black.jpeg", 
-            creator: {
-                name: 'Creator'
-            },
-            createdAt: new Date()
-        }]
-    });
+    Post.find()
+    .then(posts => {
+       console.log(posts);
+       res.status(200).json({message: 'Posts fetched', posts: posts});
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+    
 };
 
 exports.getPost = (req, res, next) => {
-    const productId = req.param.productId
+    const postId = req.params.postId;
+    
+    Post.findById(postId)
+    .then(post => {
+        console.log(post);
+        if(!post) {
+            const error = new Error('Post not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({message: 'Post fetched!', post: post});
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    })
 }
 
 exports.postPosts = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
+    if(!req.file) {
+        const error = new Error('Invalid image');
+        error.statusCode = 422;
+        throw error;
+    }
+    const image = req.file.path.replace("\\" ,"/");
+    
+    
     const errors = validationResult(req).errors;
+
     if(errors[0]) {
         const error = new Error("Post wasn't created. Error: " + errors[0].msg);
         error.statusCode = 422;
         return next(error);
     }
-    const post = new Post({title: title, image: "images/black.jpeg", content: content, creator: {name: 'Creator'}});
-
+    const post = new Post({title: title, image: image, content: content, creator: {name: 'Creator'}});
+    console.log(post);
     post.save()
     .then(result => {
         console.log(result);
@@ -48,3 +76,62 @@ exports.postPosts = (req, res, next) => {
     })
     
 };
+
+exports.editPosts = (req, res, next) => {
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let image = req.body.image;
+    if(req.file) {
+        image = req.file.path.replace("\\" ,"/");;
+    }
+    if (!image) {
+        const error = new Error('Invalid image.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const errors = validationResult(req).errors;
+
+    if(errors[0]) {
+        const error = new Error("Post wasn't created. Error: " + errors[0].msg);
+        error.statusCode = 422;
+        return next(error);
+    }
+
+    Post.findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('Post not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(image !== post.image) {
+            clearImage(post.image);
+        }
+        post.title = title;
+        post.content = content;
+        post.image = image;
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Post edited with success',
+            post: result
+        })
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+    
+};
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath)
+    fs.unlinkSync(filePath, err => {
+        throw err;
+    });
+}
--- routes/feed.js
+++ routes/feed.js
@@ -5,7 +5,17 @@ const feedController = require('../controllers/feed');
 
 router.get('/posts', feedController.getPosts);
 
-router.get('/post', feedController.getPost)
+router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', [
+    body('title').trim().isAlphanumeric().withMessage("There's a invalid character!").isLength({min: 2}).withMessage("Title's too short"),
+    body('content').trim().isAlphanumeric().withMessage("There's a invalid character!").isLength({min: 2}).withMessage("Content's too short")
+], feedController.editPosts);
+
+router.post('/post', [
+    body('title').trim().isAlphanumeric().withMessage("There's a invalid character!").isLength({min: 2}).withMessage("Title's too short"),
+    body('content').trim().isAlphanumeric().withMessage("There's a invalid character!").isLength({min: 2}).withMessage("Content's too short")
+], feedController.postPosts);
 
 router.post('/post', [
     body('title').trim().isAlphanumeric().withMessage("There's a invalid character!").isLength({min: 2}).withMessage("Title's too short"),
